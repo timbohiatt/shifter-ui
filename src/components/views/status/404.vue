0e183cefc559c9fa7a1724ec569b4f21368da9d4
Tim Hiatt <timbohiatt@gmail.com>
2022-02-18 10:14:07 +0300

    v0.3 - Added 404 & Healthz route, Reworked the Shifter Form to be multistep and prepared for error handling

---
+++ src/components/views/status/404.vue
@@ -0,0 +1,48 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+</script>
+
+<template>
+    <div>
+        <div class="glitch-wrapper">
+            <div class="glitch" data-text="404">404</div>
+            <div class="glow">404</div>
+            <p class="
+                subtitle 
+                uppercase
+                text-xs 
+                -mt-0 
+                text-left
+                tracking-normal
+                font-thin
+
+                sm:text-xs 
+                sm:-mt-2
+                sm:font-thin
+
+                md:text-xs 
+                md:-mt-4
+                md:font-thin
+
+                lg:text-md
+                lg:-mt-6
+                lg:font-thin
+
+                xl:text-lg 
+                xl:-mt-6
+                xl:font-thin
+            
+                2xl:text-xl 
+                2xl:-mt-10 
+                2xl:font-thin
+            "
+            >Failed to Shift!</p>
+        </div>
+    </div>
+</template>
+
+<style>
+
+
+</style>
--- src/routes.js
+++ src/routes.js
@@ -3,11 +3,16 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from './components/views/Home.vue'
 import Shift from './components/views/Shift.vue'
 import About from './components/views/About.vue'
+import Healthz from './components/views/status/Healthz.vue'
+import Error404 from './components/views/status/404.vue'
+
 
 const routes = [
     { path: '/', name: 'Home', component: Home}, 
     { path: '/shift', name: 'Shift', component: Shift},
-    { path: '/about', name: 'About', component: About}
+    { path: '/about', name: 'About', component: About},
+    { path: '/status/healthz', name: 'Healthz', component: Healthz},
+    { path: '/404', name: 'Healthz', component: Error404}
 ]
 const router = createRouter({
     history: createWebHistory(),
